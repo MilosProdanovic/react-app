748364e6c3a545c2acc724d026c9ee367d66c95a
MilosProdanovic <mil.prodanovic@gmail.com>
2020-03-04 13:34:11 +0100

    basic react app structure

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/bundle.js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html'
+		})
+	],
+	devServer: {
+	    port: 8080,
+	    contentBase: './dist',
+	    inline: true,
+	    hot: true
+	},
+	module: {
+	    rules: [
+	      {
+	        test: /\.css$/i,
+	        use: ['style-loader', 'css-loader'],
+	      },
+	      {
+	      	test: /\.js$/,
+	      	exclude: /node_modules/,
+	      	use: {
+	      		loader: 'babel-loader'
+	      		}
+	      	}
+    	],
+  	},
+}
